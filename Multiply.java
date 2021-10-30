c843e7cd2c51c2d19064f72b9efff38151a9c66a
prathmesh030 <prathmeshdongrikar.study@gmail.com>
2021-10-30 15:30:51 +0530

    product of 3 values

--- Multiply.java
+++ Multiply.java
@@ -2,6 +2,8 @@ class Multiply{
     public static void main(String[] args) {
         int a =15;
         int b =3;
-        System.out.print("The Product is "+(a*b));
+        int c =5;
+
+        System.out.print("The Product is "+(a*b*c));
     }
 }
