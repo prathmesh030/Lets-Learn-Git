55e95780e0dce5ca51ccc239d6f7985f73595ddf
prathmesh030 <prathmeshdongrikar.study@gmail.com>
2021-10-30 14:59:51 +0530

    print msg change

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ class Sum{
     public static void main(String[] args) {
         int a = 1;
         int b =3;
-        System.out.print("Sum is "+(a+b));
+        System.out.print("The Sum is "+(a+b));
     }
 }
